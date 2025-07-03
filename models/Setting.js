d855c53f15bbc038930fbc3b8f9bc36fb4ca2697
Amisha Prajapati <amishapra1995@gmail.com>
2025-07-03 23:39:29 +0530

    Created Setting schema

---
+++ models/Setting.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const SettingSchema = new mongoose.Schema({
+  key: { type: String, required: true, unique: true },
+  value: { type: mongoose.Schema.Types.Mixed, required: true },
+  description: String,
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now},
+  updatedAt: { type: Date, default: Date.now},
+});
+
+const Setting = mongoose.model('Setting', SettingSchema);
+
+export default Setting;
